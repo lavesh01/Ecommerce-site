f556a55165b25a07f6960783b6220a1da6e751fa
Lave <laveshbisht01@gmail.com>
2023-02-19 01:27:06 +0530

    Add Slider.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 import React from 'react'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
     </div>
   )
 }
